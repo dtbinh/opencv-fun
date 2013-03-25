77810a755eccb825a65591890719b67ed92493ec
mbarta <mbarta@redhat.com>
2013-03-25 21:50:49 +0100

    Implemented __init__, grabNextFrame and part of run functions of Compositor class.

--- 07-python_final/compositor.py
+++ 07-python_final/compositor.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
 import numpy as np
+from frame import Frame
 import cv2
+import copy
 
 
 class Compositor:
@@ -9,4 +11,48 @@ class Compositor:
     Class representing the Compositor object that takes care of taking frames,
     processing them and putting them together in the Model instance.
     """
-    pass
+
+    def __init__(self, video_source=0, debug=False):
+        """
+        Initialises an instance of Compositor class.
+
+        Parameter video_source can be number of video device or filename of
+        a video file.
+        """
+        self.debug = debug
+        self.cap = cv2.VideoCapture(video_source);
+        # TODO: Check if video stream is open (cap.isOpen())
+
+        self.frame = self.grabNextFrame()
+        self.prev_frame = None
+
+
+    def grabNextFrame(self):
+        """
+        Function reads next image from video capture device/file and returns
+        created Frame object using the image previously read.
+        """
+        # TODO: Rewrite as a iterator class !!!
+        ret, img = cap.read()
+
+        if not ret:
+            return None
+
+        return Frame(img, self.debug)
+
+
+    def run(self):
+        """
+        The main working function of Compositor class. It reads images from
+        video source in a loop and process them so that the final model is
+        created and further processed.
+        """
+        # Process first frame first:
+        self.frame.detectKeyPoints()
+
+        # TODO: fix the loop when grabNextFrame is an iterator
+        while (self.frame != None):
+            self.prev_frame = copy.deepcopy(self.frame)
+            self.frame = self.grabNextFrame()
+
+        # .... (to be finished)
