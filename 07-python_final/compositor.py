971d98e78387d92dcf0318b18f98e987bfb42405
mbarta <mbarta@redhat.com>
2013-03-25 19:40:46 +0100

    Added Compositor and VideoReader classes

---
+++ 07-python_final/compositor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+
+
+class Compositor:
+    """
+    Class representing the Compositor object that takes care of taking frames,
+    processing them and putting them together in the Model instance.
+    """
+    pass
---
+++ 07-python_final/videoreader.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import numpy as np
+import cv2
+
+
+class VideoReader:
+    """
+    Class representing the VideoReader object that manages input video stream.
+    """
+    pass
