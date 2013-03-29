962f346e7965525380cf0e1337b3f25e927882e2
Milan Barta <mbarta@redhat.com>
2013-03-29 18:07:48 +0100

    Model image expansion implemented.

--- 07-python_final/model.py
+++ 07-python_final/model.py
@@ -2,7 +2,6 @@
 
 import cv2
 import numpy as np
-import copy
 
 from frame import Frame
 import common
@@ -23,7 +22,7 @@ class Model:
         if frame == None:
             self.model = None
         else:
-            self.model = copy.deepcopy(frame)
+            self.model = frame
 
         self.act_pos = ((0,0),(0,0),(0,0),(0,0))
 
@@ -37,6 +36,44 @@ class Model:
         """
         if self.debug:
             print("Model deleted.")
+
+
+    def expand_model(self, movement):
+        """
+        Creates a new model image expanded by movement coordinates.
+        """
+        if movement[0] == 0 and movement[1] == 0:
+            return
+
+        new_img = np.zeros((self.model.img.shape[0]+abs(movement[0]), self.model.img.shape[1]+abs(movement[1]), 3), np.uint8)
+
+        x_start = x_end = y_start = y_end = 0
+
+        # Find out which directon the image was expanded to:
+        # if Y <= 0 (movement UP) => image extended UP
+        if movement[0] <= 0:
+            y_start = abs(movement[0])
+            y_end = self.model.img.shape[0]+abs(movement[0])
+        # if Y > 0 (movement DOWN) => image extended DOWN
+        else:
+            y_start = 0
+            y_end = self.model.img.shape[0]
+
+        # if X <= 0 (movement LEFT) => image extended LEFT
+        if movement[1] <= 0:
+            x_start = abs(movement[1])
+            x_end = self.model.img.shape[1]+abs(movement[1])
+        # if X > 0 (movement RIGHT) => image extended RIGHT
+        else:
+            x_start = 0
+            x_end = self.model.img.shape[1]
+
+        # Copy model image to extended one:
+        new_img[y_start:y_end, x_start:x_end] = self.model.img
+
+        # Assign extended image to image model:
+        self.model.img = new_img
+
 
 
     def add(self, frame, movement):
@@ -59,22 +96,33 @@ class Model:
 
             # TODO: use the current position as a mask for KP detection
 
-            #if self.debug:
-                #print("Mask of size {}, ones are here {}:{}, {}:{}".format((self.model.img.shape[1], self.model.img.shape[0]), self.act_pos[0][0], self.act_pos[2][0]-1, self.act_pos[0][1], self.act_pos[2][1]-1))
-
             #######################
             ## THIS DOESN'T WORK! #
             #######################
 
-            #mask = np.zeros((self.model.img.shape[1], self.model.img.shape[0]), np.uint8)
-            ## Detect KeyPoints only in current position:
-            #print("Line 64")
-            #mask[self.act_pos[0][0]:self.act_pos[2][0]-1, self.act_pos[0][1]:self.act_pos[2][1]-1] = 1
-            #print("Line 66")
-            #self.model.detectKeyPoints(mask)
-            #print("Line 68")
+            mask = np.zeros(self.model.img.shape[:2], np.uint8)
+
+            if self.debug:
+                print("Mask of size {} created.".format(self.model.img.shape[:2]))
+                print("-> the ones are here: {}:{}, {}:{}".format(self.act_pos[0][0], self.act_pos[2][0]-1, self.act_pos[0][1], self.act_pos[2][1]-1))
+
+            # Detect KeyPoints only in current position:
+            mask[self.act_pos[0][0]:self.act_pos[2][0]-1, self.act_pos[0][1]:self.act_pos[2][1]-1] = 1
+            self.model.detectKeyPoints(mask)
+
+            self.expand_model(movement)
+
+            if self.debug:
+                cv2.imshow("x", self.model.img)
+                cv2.waitKey(0)
+                print("New img size = {}".format(self.model.img.shape))
+
+            if self.debug:
+                print("Number of detected KP's: {}".format(len(self.model.kp)))
+
             ## TODO: theese coordinates will have to be warped and corrected over time
-            self.act_pos = tuple((item[0]+movement[0], item[1]+movement[1]) for item in self.act_pos)
+            ## TODO: don't change the act_pos just yet ...
+            #self.act_pos = tuple((item[0]+movement[0], item[1]+movement[1]) for item in self.act_pos)
 
 
             # TODO: match the keypoints and compute homography out of them
